c7e6a8f6bf229307cc78456ff1c647b01da64372
yena <50291995+nyj001012@users.noreply.github.com>
2023-12-16 15:18:02 +0900

    feat: tutorial 03 has been done

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 
 from . import views  # 현재 패키지에서 views 모듈을 가져온다.
 
+app_name = "polls"  # URL 패턴의 이름공간을 polls로 지정한다.
+
 urlpatterns = [
-    path("", views.index, name="index"),  # views.py에서 정의한 index 함수를 호출한다. 즉 views.index를 루트, 인덱스 페이지로 사용한다.
+	path("", views.index, name="index"),  # views.py에서 정의한 index 함수를 호출한다. 즉 views.index를 루트, 인덱스 페이지로 사용한다.
+	path("<int:question_id>/", views.detail, name="detail"),
+	path("<int:question_id>/results/", views.results, name="results"),
+	path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,15 +1,66 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpRequest, Http404
+from django.template import loader
+from .models import Question
 
 
 def index(request):
-    """
-    polls 앱의 index 페이지를 반환한다.
+	"""
+	polls 앱의 index 페이지를 반환한다.
 
-    Args:
-        request: 클라이언트로부터 받은 요청
+	Args:
+		request: 클라이언트로부터 받은 요청
 
-    Returns:
-        HttpResponse: index 페이지
-    """
-    return HttpResponse("Hello, world. You're at the polls index.")
+	Returns:
+		HttpResponse: index 페이지
+	"""
+	latest_question_list = Question.objects.order_by("-pub_date")[:5]
+	template = loader.get_template("polls/index.html")
+	context = {
+		"latest_question_list": latest_question_list,
+	}
+	return HttpResponse(template.render(context, request))
+
+
+def detail(request, question_id):
+	"""
+	question_id에 해당하는 질문의 상세 페이지를 반환한다.
+
+	Args:
+		request (HttpRequest): 클라이언트로부터 받은 요청
+		question_id (int): 질문의 id
+
+	Returns:
+		HttpResponse: question_id에 해당하는 질문의 상세 페이지
+	"""
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, "polls/detail.html", {"question": question})
+
+
+def results(request, question_id):
+	"""
+	question_id에 해당하는 질문의 결과 페이지를 반환한다.
+
+	Args:
+		request (HttpRequest): 클라이언트로부터 받은 요청
+		question_id (int): 질문의 id
+
+	Returns:
+		HttpResponse: question_id에 해당하는 질문의 결과 페이지
+	"""
+	response = "You're looking at the results of question %s."
+	return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+	"""
+	question_id에 해당하는 질문에 투표한다.
+
+	Args:
+		request (HttpRequest): 클라이언트로부터 받은 요청
+		question_id (int): 질문의 id
+
+	Returns:
+		HttpResponse: question_id에 해당하는 질문에 투표하는 페이지
+	"""
+	return HttpResponse("You're voting on question %s." % question_id)
