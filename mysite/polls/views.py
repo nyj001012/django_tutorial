5902481abd723a3195e9298b4e48bb7bfd3f8755
yena <50291995+nyj001012@users.noreply.github.com>
2023-12-18 12:27:20 +0900

    feat: tutorial 04 has been done

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,59 @@
+from deprecated import deprecated
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpRequest, Http404
+from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
 from django.template import loader
-from .models import Question
+from django.urls import reverse
+from django.views import generic  # F() 사용: F() 객체를 이용하여 race condition을 방지한다.
+from django.db.models import F
+from .models import Question, Choice
 
 
+class IndexView(generic.ListView):
+	"""
+	Question 모델의 최근 5개 질문을 보여주는 뷰
+
+	Attributes:
+		template_name: 템플릿 파일의 경로
+		context_object_name: 템플릿 파일에서 사용할 컨텍스트 변수의 이름
+	"""
+	template_name = "polls/index.html"
+	context_object_name = "latest_question_list" # 템플릿 파일에서 사용할 컨텍스트 변수의 이름
+
+	def get_queryset(self):
+		"""
+		최근 5개의 질문을 반환한다.
+
+		Returns:
+			QuerySet: 최근 5개의 질문
+		"""
+		return Question.objects.order_by("-pub_date")[:5]
+
+
+class DetailView(generic.DetailView):
+	"""
+	Question 모델의 상세 페이지를 보여주는 뷰
+
+	Attributes:
+		model: 사용할 모델
+		template_name: 템플릿 파일의 경로
+	"""
+	model = Question
+	template_name = "polls/detail.html"
+
+
+class ResultsView(generic.DetailView):
+	"""
+	Question 모델의 결과 페이지를 보여주는 뷰
+
+	Attributes:
+		model: 사용할 모델
+		template_name: 템플릿 파일의 경로
+	"""
+	model = Question
+	template_name = "polls/results.html"
+
+
+@deprecated("제너릭 뷰를 사용한다.")
 def index(request):
 	"""
 	polls 앱의 index 페이지를 반환한다.
@@ -22,6 +72,7 @@ def index(request):
 	return HttpResponse(template.render(context, request))
 
 
+@deprecated("제너릭 뷰를 사용한다.")
 def detail(request, question_id):
 	"""
 	question_id에 해당하는 질문의 상세 페이지를 반환한다.
@@ -37,6 +88,7 @@ def detail(request, question_id):
 	return render(request, "polls/detail.html", {"question": question})
 
 
+@deprecated("제너릭 뷰를 사용한다.")
 def results(request, question_id):
 	"""
 	question_id에 해당하는 질문의 결과 페이지를 반환한다.
@@ -48,8 +100,8 @@ def results(request, question_id):
 	Returns:
 		HttpResponse: question_id에 해당하는 질문의 결과 페이지
 	"""
-	response = "You're looking at the results of question %s."
-	return HttpResponse(response % question_id)
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, "polls/results.html", {"question": question})
 
 
 def vote(request, question_id):
@@ -63,4 +115,22 @@ def vote(request, question_id):
 	Returns:
 		HttpResponse: question_id에 해당하는 질문에 투표하는 페이지
 	"""
-	return HttpResponse("You're voting on question %s." % question_id)
+	question = get_object_or_404(Question, pk=question_id)
+	try:
+		selected_choice = question.choice_set.get(pk=request.POST["choice"])
+	# F() 예제 1: selected_choice.filter(pk=request.POST["choice"]).update(votes=F("votes") + 1)
+	except (KeyError, Choice.DoesNotExist):
+		return render(
+			request,
+			"polls/detail.html",
+			{
+				"question": question,
+				"error_message": "You didn't select a choice.",
+			},
+		)
+	else:
+		selected_choice.votes = F("votes") + 1  # F() 예제 2
+		selected_choice.save()
+		# POST 데이터를 정상적으로 처리한 후에는 항상 HttpResponseRedirect를 반환해야 한다.
+		# 이렇게 하면 사용자가 브라우저의 "뒤로" 버튼을 눌렀을 때 데이터가 두 번 저장되는 것을 방지할 수 있다.
+		return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
